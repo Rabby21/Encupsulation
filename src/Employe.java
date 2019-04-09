fd8988a89c757e7f105e12fa0e29a5deac7706a7
Golam Rabby <golam.rabby@ntdac.naztech.local>
2019-04-09 13:09:39 +0600

    initial commit

---
+++ src/Employe.java
@@ -0,0 +1,9 @@
+
+
+public class Employe {
+	public static String name="Rabby";
+	public static int id=101;
+	
+	//public void showdata(String)
+
+}
---
+++ src/Encupsulation.java
@@ -0,0 +1,10 @@
+public class Encupsulation {
+	public static void main(String[] args) {
+		//Employe emp=new Employe();
+		String y=Employe.name;
+		System.out.println(y);
+	    int z=Employe.id;
+	    System.out.println(z);
+	}
+
+}
